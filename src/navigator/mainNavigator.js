a5b376b43b2cc6ab26d2847d2684f3c7bb072cf8
evan-carlsen <evan@crowdbotics.com>
2020-11-13 13:35:10 -0800

    Update screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -1,29 +1,26 @@
-import { createAppContainer } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import {createDrawerNavigator} from 'react-navigation-drawer';
-
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+// import BlankScreen114504Navigator from '../features/BlankScreen114504/navigator';
 // import BlankScreen014493Navigator from '../features/BlankScreen014493/navigator';
 import EmailAuthNavigator from "../features/EmailAuth/navigator";
-
 /**
  * new navigators can be imported here
  */
-
-const AppNavigator = {
-
-    //@BlueprintNavigationInsertion
-// BlankScreen014493: { screen: BlankScreen014493Navigator },
-EmailAuth: {screen: EmailAuthNavigator},
-  
-    /** new navigators can be added here */
-    SplashScreen: {
-      screen: SplashScreen
-    }
+const switchScreens = {
+  EmailAuthNavigator,
+  DrawerAppNavigator,
+  SplashScreen: {
+    screen: SplashScreen
+  }
 };
-
+const switchSettings = {
+  // initialRouteName: 'TestScreen',
+  //backBehavior:'initialRoute'
+};
 const DrawerAppNavigator = createDrawerNavigator(
   {
     ...AppNavigator,
@@ -32,7 +29,6 @@ const DrawerAppNavigator = createDrawerNavigator(
     contentComponent: SideMenu
   },
 );
-
-const AppContainer = createAppContainer(DrawerAppNavigator);
-
+const AppNavigator = createSwitchNavigator(switchScreens, switchSettings);
+const AppContainer = createAppContainer(AppNavigator);
 export default AppContainer;
