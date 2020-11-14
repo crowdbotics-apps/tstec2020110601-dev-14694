ddf647dee7f65ea8b3488beef83c06bb9f2aacd6
evan-carlsen <evan@crowdbotics.com>
2020-11-13 16:45:05 -0800

    Update screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,29 +5,20 @@ import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 // import BlankScreen114504Navigator from '../features/BlankScreen114504/navigator';
-// import BlankScreen014493Navigator from '../features/BlankScreen014493/navigator';
+import BlankScreen014493Navigator from '../features/BlankScreen014493/navigator';
 import EmailAuthNavigator from "../features/EmailAuth/navigator";
+import Sign from '../features/EmailAuth/screens/index'
 /**
  * new navigators can be imported here
  */
-
 // Add this to make your authentication manditory before displaying the rest of the application
-const DrawerScreens={
- // to make non-manditory uncomment this 
- //EmailAuthNavigator,
-
+const DrawerScreens = {
+  // to make non-manditory uncomment this
+  //  Sign,
+  BlankScreen014493Navigator:{
+    screen:BlankScreen014493Navigator
+  }
 }
-const switchScreens = {
-  // comment this to make it non-manditory
-  EmailAuthNavigator, 
-  DrawerAppNavigator
-};
-const switchSettings = {
-  // initialRouteName: 'TestScreen',
-  //backBehavior:'initialRoute'
-};
-// end auth updates
-
 const DrawerAppNavigator = createDrawerNavigator(
   {
     ...DrawerScreens,
@@ -36,6 +27,18 @@ const DrawerAppNavigator = createDrawerNavigator(
     contentComponent: SideMenu
   },
 );
+const switchScreens = {
+  // comment this to make it non-manditory
+  EmailAuth: {
+    screen: EmailAuthNavigator,
+  },
+  DrawerAppNavigator,
+};
+const switchSettings = {
+  // initialRouteName: 'TestScreen',
+  //backBehavior:'initialRoute'
+};
+// end auth updates
 // auth updates to be included
 const SwitchNavigator = createSwitchNavigator(switchScreens, switchSettings);
 // end updates
